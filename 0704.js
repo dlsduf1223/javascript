1b10e4da6af6f8c10a047600298b74012e45c3e0
Choi <dlsduf1223@nate.com>
2022-07-04 19:56:40 +0900

    0704-3

--- 0704.js
+++ 0704.js
@@ -56,3 +56,44 @@ function 함수({ name, age }) {
 import 가져올거 from "경로";
 
 // export default는 딱한번만 쓸 수 있다.
+// 여러개 하려면
+export { a, b };
+export var a = 10; //변수를 만들면서 export해줌
+export default c; // 위의 것과 동시에 사용 가능하다.
+
+import c, { a, b } from "library.js"; // 이렇게 써야함..
+
+import { a as 별명 } from "library.js"; // a의 변수명을 변경하여 쓰기 위함..
+
+//default를 제외한 모든 변수를 한번에 import하고 싶다면?
+import * as 작명 from "library.js";
+console.log(작명.a);
+console.log(작명.c); //c는 export default라 불가능 import에 따로 더 추가해줘야함...
+
+<script src="library.js"></script>; // 위와 같은 코드지만 호환성이 좋긴하다.
+
+/*--------------------------------------------------------------------------------------------------*/
+// 비동기 함수들을 순차적으로(동기적)으로 실행하려면??
+
+// 콜백함수를 써보자. 파라미터 자리에 콜백함수를 넣는것임..
+function 첫째함수() {
+  console.log(1);
+}
+
+function 둘째함수() {
+  console.log(2);
+}
+
+첫쨰함수();
+둘째함수(); //이러면 순서대로 실행이 되지 않을수도 있음. because of stack, queue 개념때문에
+
+function 첫째함수(둘째함수) {
+  console.log(1);
+  둘째함수();
+}
+
+function 둘째함수() {
+  console.log(2);
+}
+
+첫째함수(둘째함수); //이러면 무조건 순차적으로 실행 됨
